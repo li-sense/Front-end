42f5026952254c39d4c6c5f07a94c6667563b203
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2022-10-08 17:47:18 -0300

    primeira etapa dos cards finalizada

--- src/_components/Card/Card.js
+++ src/_components/Card/Card.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import './Card.css'
+import Image from "./img/engine.jpg";
+import Arrow from "./img/arrow.png"
+import Star from "./img/star.png"
 
 export default function Card(){
   return (
     <>
-    card
+      <div id="content">
+        <div class="card" id="one">
+          <div id='fileIcon'></div>
+          <img src={Image} class="view" alt="" />
+
+          <p class="title">Nome</p>
+          <p class="price">R$200,00</p>
+          <p class="desc">
+            Lorem ipsum dolor sit aconsectetur adipisicing elit. Liobcaecati
+            eligendi tempora nesciunt.
+          </p>
+          <div class="arrow">
+            <img src={Arrow} id="ar"></img>
+            <img src={Arrow} id="arr"></img>
+          </div>
+          {/* <img id="fav" src={Star}></img> */}
+
+
+        </div>
+      </div>
     </>
   );
 }
