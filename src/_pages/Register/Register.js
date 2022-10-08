a3c84bb14305bdc145bc8095655cad0925448ca4
TaatiShibata <taati.shibata@gmail.com>
2022-10-08 20:55:08 -0300

    Atualização da tela de cadastro

--- src/_pages/Register/Register.js
+++ src/_pages/Register/Register.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Form, useNavigate } from 'react-router-dom';
 import './styles.css';
+import Logocentral from "../../_assets/img/logoredonda.png"
+import {AiOutlineUserAdd } from 'react-icons/ai' ; 
+import {FiMail} from 'react-icons/fi' ;
+import {BiLockAlt} from 'react-icons/bi' ;
 export default function Register() {
   const navigate = useNavigate();
   return (
     <div className='container-geral'>
       <div className='container-texto'>
-        <h2>Crie a sua conta</h2>
-        <span>"Facilite seus processos." <em>LI-SENSE</em>, onde você é importante</span>
+      <h1>Cadastro</h1>
+        <span>"Facilite seus processos." <em>LI-SENSE</em>, onde você é importante.</span>
       </div>
       <div className='container-panel'>
-        <h1>Cadastro</h1>
-
+       
         <div className='panel-form'>
-          <div className='panel-row'>
-            <label forHtml='nome-input'>Nome</label>
-            <input type='text' placeholder='Digite seu nome'
+          {/* <div className='panel-row'>
+            <span className= 'span-input'>Nome Completo</span>
+            <input className='mb-3'type='text' placeholder='Digite seu nome'
               name='nome-input' id='nome-input' />
           </div>
           <div className='panel-row'>
@@ -25,24 +28,69 @@ export default function Register() {
           </div>
           <div className='panel-row'>
             <label forHtml='cpf-input'>CPF</label>
-            <input type='text' placeholder='Digite seu CPF'
+            <input className='mb-3' type='text' placeholder='Digite seu CPF'
               name='cpf-input' id='cpf-input'/>
           </div>
           <div className='panel-row'>
             <label forHtml='tel-input'>Telefone</label>
-            <input type='text' placeholder='Digite seu telefone'
+            <input className='mb-3' type='text' placeholder='Digite seu telefone'
               name='tel-input' id='tel-input'/>
-          </div>
+          </div> */}
+            <div className="mb-3">
+              <span className='span-input'>Nome Completo</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Nome Completo"
+                />
+              <AiOutlineUserAdd size={24} className='icon' />
+              </div>
+            </div>
+            <div className="mb-3">
+              <span className='span-input'>E-mail</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="mail@mail.com"
+                />
+            <FiMail size={24} className='icon' />
+              </div>
+            </div>
+            <div className="mb-3">
+              <span className='span-input'>Senha</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Senha"
+                />
+          <BiLockAlt size={24} className='icon' />
+              </div>
+            </div>
+            <div className="mb-3">
+              <span className='span-input'>Repetir Senha</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Repetir senha"
+                />
+          <BiLockAlt size={24} className='icon' />
+              </div>
+            </div>
         </div>
       
         <div className='container-button'>
-          <button type='submit' onClick={() => {navigate('/')}}>Concluir</button>
-          <button type='reset'>Reiniciar</button>
+          <button type='submit'className='btn' onClick={() => {navigate('/')}}>Cadastar</button>
+          
           {/* 
             o botão de reiniciar por enquanto não tem funcionalidade porque 
             será mais facil implementa-lo quando estivermos usando hooks e states  
           */}
         </div>
+        <img className='img1' src= {Logocentral}></img>
       </div>
     </div>
   );
