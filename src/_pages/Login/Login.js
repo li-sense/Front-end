053481bdadf576fd2f6a680a953cb71ef9b96559
JhonataBast <58173651+JhonataBast@users.noreply.github.com>
2022-10-08 12:42:58 -0300

    tela-login

--- src/_pages/Login/Login.js
+++ src/_pages/Login/Login.js
@@ -1,15 +1,71 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import './Login.css';
+import Logocentral from "../../_assets/img/logoredonda.png";
+import { FaLock } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { HiOutlineMail } from "react-icons/hi";
+
+// import Logobackground from "../../_assets/img/tela6.png";
+// import Logofinal from "../../_assets/img/logoc.png";
+
 export default function Login() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   return (
-    <>
-      <div className='flex flex-col'>
-        tela de login
-        <button className='p-4 border w-36' onClick={() => {navigate('/register')}}>
-          cadastro
-        </button>
+
+    <div className="container-geral">
+      <div className="containerTexto">
+        <div className="textoLogin">
+          <img className="img1" src={Logocentral} />
+          <h2>Seja Bem-vindo!</h2>
+          <span>"Oportunidades não surgem. É você que as cria" LI-SENSE onde você é importante."</span>
+        </div>
       </div>
-    </>
-  )
+      <div className="telalogin">
+        <div className="containerLogin">
+          <h1>Login</h1>
+          <form>
+            <div className="mb-3">
+              <span className='span-input'>E-mail</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="mail@mail.com"
+                />
+                <HiOutlineMail size={24} className='icon' />
+              </div>
+            </div>
+            <div className="mb-3">
+              <span className='span-input'>Password</span>
+              <div className='container-input'>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="**********"
+                />
+                <FaLock size={20} className='icon' />
+              </div>
+            </div>
+            <p className="esq">Esqueceu a senha?</p>
+            <button type="submit" className="btn"> Entrar</button>
+          </form>
+          <p className="cadastrar">
+            <a>Criar uma conta</a>
+          </p>
+          <p className="entarg">
+            <a>Ou</a>
+          </p>
+          <button type="submit" className="btn1">
+            <div className='content-btn1'>
+              <FcGoogle size={24} /> <span>Sign in with Google</span>
+            </div>
+          </button>
+        </div>
+      </div>
+
+
+    </div>
+
+
+  );
 }
