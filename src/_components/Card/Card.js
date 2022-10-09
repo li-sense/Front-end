495483fc2023a39fabf1345f0910fbd19ec963c0
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2022-10-08 21:17:58 -0300

    antes de adc a fonte

--- src/_components/Card/Card.js
+++ src/_components/Card/Card.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import './Card.css'
 import Image from "./img/engine.jpg";
-import Arrow from "./img/arrow.png"
-import Star from "./img/star.png"
+import Arrow from "./img/arrow.png";
+import { IconContext } from "react-icons";
+import { VscFile } from "react-icons/vsc";
+
 
 export default function Card(){
   return (
-    <>
-      <div id="content">
-        <div class="card" id="one">
-          <div id='fileIcon'></div>
-          <img src={Image} class="view" alt="" />
-
-          <p class="title">Nome</p>
-          <p class="price">R$200,00</p>
-          <p class="desc">
-            Lorem ipsum dolor sit aconsectetur adipisicing elit. Liobcaecati
-            eligendi tempora nesciunt.
-          </p>
-          <div class="arrow">
-            <img src={Arrow} id="ar"></img>
-            <img src={Arrow} id="arr"></img>
+  <IconContext.Provider value={{ color: 'white', size: '12px'}}>
+    <div id="content">
+      <div class="card" id="one">
+        <div id='icon'>
+          <div class='wrapper'>
+          < VscFile /> 
           </div>
-          {/* <img id="fav" src={Star}></img> */}
+        </div> 
+        <img src={Image} class="view" alt="" />
 
-
+        <p class="title">Nome</p>
+        <p class="price">R$200,00</p>
+        <p class="desc">
+          Lorem ipsum dolor sit aconsectetur adipisicing elit. Liobcaecati
+          eligendi tempora nesciunt.
+        </p>
+        <div class="arrows">
+          <img src={Arrow} id="upperArrow"></img>
+          <img src={Arrow} id="lowerArrow"></img>
         </div>
       </div>
-    </>
+    </div>
+    </IconContext.Provider>
   );
 }
