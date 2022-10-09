4989ade1f809e9be3ebecfe4a3e9676384b70f9d
||ȼhaiɳ <sfgfffs@gmail.com>
2022-10-08 23:19:58 -0300

    add filters and set render all cards in home

--- src/_components/Card/Card.js
+++ src/_components/Card/Card.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import './Card.css'
-import Image from "./img/engine.jpg";
 import Arrow from "./img/arrow.png";
-import { IconContext } from "react-icons";
 import { VscFile } from "react-icons/vsc";
 
 
-export default function Card(){
+export default function Card(props) {
+  const { data } = props;
   return (
-  <IconContext.Provider value={{ color: 'white', size: '12px'}}>
-    <div id="content">
+    <>
+      <div className='flex flex-col card overflow-hidden m-4'>
+        <div className='w-full flex justify-end items-center'>
+          <div className='vsicon flex items-center justify-center'>
+            < VscFile size={20} />
+          </div>
+        </div>
+        <div className='view flex flex-col items-center'>
+          <img src={data.url} class="img" alt="" />
+          <div className='flex items-center'>
+            <div className='w-[80%] ml-4'>
+              <p class="text-2xl text-white">{data.name}</p>
+              <p class="price">{data.price}</p>
+              <p class="desc">
+                {data.description}
+              </p>
+            </div>
+            <div class="arrows h-full w-[20%] -mt-8 -ml-8">
+              <img src={Arrow} className="arrow"></img>
+              <img src={Arrow} className="arrow"></img>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+/*
+<IconContext.Provider value={{ color: 'white', size: '12px'}}>
+    <div id="content" style={{backgroundImage: ''}}>
       <div class="card" id="one">
         <div id='icon'>
-          <div class='wrapper'>
-          < VscFile /> 
+          <div class='wrapper flex items-center mt-2'>
+          < VscFile size={30}/> 
           </div>
         </div> 
         <img src={Image} class="view" alt="" />
@@ -24,12 +53,13 @@ export default function Card(){
           Lorem ipsum dolor sit aconsectetur adipisicing elit. Liobcaecati
           eligendi tempora nesciunt.
         </p>
-        <div class="arrows">
-          <img src={Arrow} id="upperArrow"></img>
-          <img src={Arrow} id="lowerArrow"></img>
-        </div>
+       
       </div>
     </div>
     </IconContext.Provider>
-  );
-}
+
+ <div class="arrows">
+        <img src={Arrow} id="upperArrow"></img>
+          <img src={Arrow} id="lowerArrow"></img>
+        </div>
+*/
--- src/_pages/Home/Home.js
+++ src/_pages/Home/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Components } from '../../_components/Components';
+import './Home.css';
+import { data } from '../../FakeData'
 export default function Home() {
   return (
     <>
-      Home
-      <div>
-        <Components.Card />
+      <div className='flex flex-col mb-20 items-center '>
+       <div className='mt-10 mb-8'>
+       <Components.Filters />
+       </div>
+      <div className='flex flex-wrap justify-center'>
+      {
+        data.map((_value, key) => (
+          <Components.Card data={_value}/>
+        ))
+       }
+      </div>
+       <button className='mt-4 filter'>
+        Ver mais
+       </button>
       </div>
     </>
   );
