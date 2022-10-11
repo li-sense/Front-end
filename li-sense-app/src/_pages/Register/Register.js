152aefdae655464e464cef81528a5699639ef3d5
||ȼhaiɳ <sfgfffs@gmail.com>
2022-10-11 19:52:16 -0300

    set default app for dev

--- li-sense-app/src/_pages/Register/Register.js
+++ li-sense-app/src/_pages/Register/Register.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { Form, useNavigate } from "react-router-dom";
-import "./styles.css";
-import Logocentral from "../../_assets/img/logoredonda.png";
-import { AiOutlineUserAdd } from "react-icons/ai";
-import { FiMail } from "react-icons/fi";
-import { BiLockAlt } from "react-icons/bi";
+import React from 'react';
+import { Form, useNavigate } from 'react-router-dom';
+import './styles.css';
+import Logocentral from "../../_assets/img/logoredonda.png"
+import {AiOutlineUserAdd } from 'react-icons/ai' ; 
+import {FiMail} from 'react-icons/fi' ;
+import {BiLockAlt} from 'react-icons/bi' ;
 export default function Register() {
   const navigate = useNavigate();
   return (
-    <div className="container-geral">
-      <div className="container-texto">
-        <h1>Cadastro</h1>
-        <span>
-          "Facilite seus processos." <em>LI-SENSE</em>, onde você é importante.
-        </span>
+    <div className='container-all'>
+      <div className='container-texto'>
+      <img className='img2' src= {Logocentral}></img>
+      <h1>Cadastro</h1>
+        <span>"Facilite seus processos." <em>LI-SENSE</em>, onde você é importante.</span>
       </div>
-      <div className="container-panel">
-        <div className="panel-form">
+      <div className='container-panel'>
+       
+        <div className='panel-form'>
           {/* <div className='panel-row'>
             <span className= 'span-input'>Nome Completo</span>
             <input className='mb-3'type='text' placeholder='Digite seu nome'
@@ -37,69 +37,61 @@ export default function Register() {
             <input className='mb-3' type='text' placeholder='Digite seu telefone'
               name='tel-input' id='tel-input'/>
           </div> */}
-          <div className="mb-3">
-            <span className="span-input">Nome Completo</span>
-            <div className="container-input">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Nome Completo"
-              />
-              <AiOutlineUserAdd size={24} className="icon" />
+            <div className="mb-3">
+              <span className='span-input1'>Nome completo</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="nome completo"
+                />
+              <AiOutlineUserAdd size={24} className='icon-register' />
+              </div>
             </div>
-          </div>
-          <div className="mb-3">
-            <span className="span-input">E-mail</span>
-            <div className="container-input">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="mail@mail.com"
-              />
-              <FiMail size={24} className="icon" />
+            <div className="mb-3">
+              <span className='span-input1'>Email</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="mail@mail.com"
+                />
+            <FiMail size={24} className='icon-register' />
+              </div>
             </div>
-          </div>
-          <div className="mb-3">
-            <span className="span-input">Senha</span>
-            <div className="container-input">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Senha"
-              />
-              <BiLockAlt size={24} className="icon" />
+            <div className="mb-3">
+              <span className='span-input1'>Senha</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="senha"
+                />
+          <BiLockAlt size={24} className='icon-register' />
+              </div>
             </div>
-          </div>
-          <div className="mb-3">
-            <span className="span-input">Repetir Senha</span>
-            <div className="container-input">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Repetir senha"
-              />
-              <BiLockAlt size={24} className="icon" />
+            <div className="mb-3">
+              <span className='span-input1'>Repetir senha</span>
+              <div className='container-input'>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="repetir senha"
+                />
+          <BiLockAlt size={24} className='icon-register' />
+              </div>
             </div>
-          </div>
         </div>
-
-        <div className="container-button">
-          <button
-            type="submit"
-            className="btn"
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            Cadastar
-          </button>
-
+      
+        <div className='container-button'>
+          <button type='submit'className='btn-cad' onClick={() => {navigate('/')}}>Cadastar</button>
+          
           {/* 
             o botão de reiniciar por enquanto não tem funcionalidade porque 
             será mais facil implementa-lo quando estivermos usando hooks e states  
           */}
         </div>
-        <img className="img1" src={Logocentral}></img>
+        
       </div>
     </div>
   );
