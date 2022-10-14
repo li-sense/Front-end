5003076bbf31cd44ef8eeb307a50b664c4557e9e
JhonataBast <58173651+JhonataBast@users.noreply.github.com>
2022-10-14 12:07:54 -0300

    Tela recuperar senha

--- li-sense-app/src/_components/RecoveryPassword/SendEmailRecovery/SendEmailRecovery.js
+++ li-sense-app/src/_components/RecoveryPassword/SendEmailRecovery/SendEmailRecovery.js
@@ -1,15 +1,36 @@
 import React from "react";
+import './styles.css';
+import {FiMail} from 'react-icons/fi' ;
 
 export default function SendEmailRecovery(props) {
-  const { setIsReceivedEmail } = props
+   const { setIsReceivedEmail } = props
   return (
     <>
-    <div className="flex flex-col">
-      Componente de envio do email
-      <button onClick={() => { setIsReceivedEmail(true) }} className='border p-4 m-4 w-48'>
-              Enviar link
-            </button>
-    </div>
+    
+          
+      <form className="card-link">
+            
+              <label htmlFor="send" className='span-send-email'>E-mail para recuperação de senha</label>
+             <div className="input-wrapper">
+                <input
+                  id="send"
+                  type="email"
+                  className="form-control"
+                  placeholder="mail@mail.com"
+                />
+                <span className="input-icon"><FiMail size={24} className='icon-register1'/></span>
+            
+              </div>
+
+        <button  className="btn-link" onClick={() => { setIsReceivedEmail(true) }}>
+            Enviar link
+        </button>
+    
+      </form>
+      
+      
+      
+   
     </>
   )
 }
--- li-sense-app/src/_pages/RecoveryPassword/RecoveryPassword.js
+++ li-sense-app/src/_pages/RecoveryPassword/RecoveryPassword.js
@@ -1,25 +1,29 @@
 import React from "react";
 import RegisterNewPassword from "../../_components/RecoveryPassword/RegisterNewPassword/RegisterNewPassword";
 import SendEmailRecovery from "../../_components/RecoveryPassword/SendEmailRecovery/SendEmailRecovery";
+import './RecoveryPassword.css';
 export default function RecoveryPassword() {
   const [isReceivedEmail, setIsReceivedEmail] = React.useState(false);
   return (
     <>
-    <div>
-      <h1>Redefinir senha</h1>
-      <a>oi</a>
-    </div>
-      {
-        !isReceivedEmail ?
-          <div>
-          <SendEmailRecovery setIsReceivedEmail={setIsReceivedEmail}/>
-          
-          </div>
-          : <div>
-           <RegisterNewPassword setIsReceivedEmail={setIsReceivedEmail}/>
-          
-          </div>
-      }
+    
+      <div className="container-send">
+        <h1>Redefinir senha</h1>
+        {
+          !isReceivedEmail ?
+            <div>
+            <SendEmailRecovery setIsReceivedEmail={setIsReceivedEmail}/>
+            
+            </div>
+            : <div>
+            <RegisterNewPassword setIsReceivedEmail={setIsReceivedEmail}/>
+            
+            </div>
+        }
+      </div>  
+        
+  
+     
     </>
   )
 }
