c22eacea538ae2597de5505af63c5726b4c94765
||ȼhaiɳ <sfgfffs@gmail.com>
2022-10-23 13:15:38 -0300

    add axios component and set user login

--- li-sense-app/src/_pages/Login/Login.js
+++ li-sense-app/src/_pages/Login/Login.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 import Logocentral from "../../_assets/img/logoredonda.png";
+import LisenseContext from '../../_context/LisenseContext';
 import { FaLock } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { HiOutlineMail } from "react-icons/hi";
-import { Link } from 'react-router-dom';
+import userLogin from '../../_service/userLogin';
 
 // import Logobackground from "../../_assets/img/tela6.png";
 // import Logofinal from "../../_assets/img/logoc.png";
 
 export default function Login() {
    const navigate = useNavigate();
-  return (
+   const [userData, setUserData] = React.useState({email: '', password: ''});
+   const { setUser } = React.useContext(LisenseContext);
 
+  return (
     <div className="container-geral">
       <div className="containerTexto">
         <div className="textoLogin">
@@ -34,6 +37,7 @@ export default function Login() {
                   type="email"
                   className="form-control"
                   placeholder="mail@mail.com"
+                  onChange={(event) => {setUserData({email: event.target.value, password:userData.password})}}
                 />
                 <HiOutlineMail size={24} className='icon' />
               </div>
@@ -45,12 +49,13 @@ export default function Login() {
                   type="password"
                   className="form-control"
                   placeholder="**********"
+                  onChange={(event) => {setUserData({email: userData.email, password:event.target.value})}}
                 />
                 <FaLock size={20} className='icon' />
               </div>
             </div>
             <p className="esq" onClick={() => {navigate('/recovery')}}>Esqueceu a senha?</p>
-            <button type="submit" className="btn"> Entrar</button>
+            <button type="submit" className="btn" onClick={() => {userLogin(userData, setUser)}}> Entrar</button>
           </form>
           <p className="cadastrar">
             <a onClick={() => {navigate('/register')}}>Criar uma conta</a>
