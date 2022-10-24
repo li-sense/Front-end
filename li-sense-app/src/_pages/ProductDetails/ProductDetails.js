c745be81ee7bd98804b410a931f2d84807bef0e6
||ȼhaiɳ <sfgfffs@gmail.com>
2022-10-24 09:07:29 -0300

    base componente

--- li-sense-app/src/_pages/ProductDetails/ProductDetails.js
+++ li-sense-app/src/_pages/ProductDetails/ProductDetails.js
@@ -1,12 +1,24 @@
 import React from "react";
 import LisenseContext from "../../_context/LisenseContext";
+import './Product.css';
 export default function ProductDetails() {
   const { currentProduct } = React.useContext(LisenseContext);
   console.log(currentProduct);
   return (
     <>
-    <div>
-      pagina de detalhes
+    <div className="w-full h-full flex flex-col">
+      <div className="flex items-center justify-around w-full">
+      <div className="flex flex-col mr-4 items-center justify-center description">
+        <h1>{currentProduct.name}</h1>
+        <p className="border p-4 w-48 cursor-pointer">
+          {currentProduct.price}
+        </p>
+        <p>{currentProduct.description}</p>
+      </div>
+      <div className="image">
+      <img className="bg" src={currentProduct.url}/>
+      </div>
+      </div>
     </div>
     </>
   )
