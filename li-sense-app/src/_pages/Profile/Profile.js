4bf9cb5d11f0430a5673cc1ed837902275d9ea03
Zokyn <gustavo.casaes@mail.uft.edu.br>
2022-10-24 17:57:54 -0300

    Feature/S02/T74 - refactor: definindo classes em tailwind

--- li-sense-app/src/_pages/Profile/Profile.js
+++ li-sense-app/src/_pages/Profile/Profile.js
@@ -12,77 +12,93 @@ export default function Profile(props){
       <div className='container-text'>
         <h1>Painel do Usuário</h1>
       </div>
-      <div className='container-panel'> 
-        <div className='panel-form'>
-          <div className='form-group'>
-            <div className="form-row">
-              <span className='span-input'>Nome completo</span>
-              <div className='container-input'>
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder={`${nome}`}
-                />
-              </div>
+      <div className='mx-auto w-full max-w-[450px]'> 
+        <div className='-mx-auto flex flex-wrap panel-form'>
+          <div className='w-full px-3 sm:w-1/2'>
+            <div className="mb-5">
+              <label 
+                forhmtl='username'
+                className='mb-3 block text-base font-medium'>
+                  Nome completo
+              </label>
+              <input
+                name='username'
+                className='form-control'
+                placeholder={`${nome}`}
+              />
             </div>
-            <div className='desc-input'>
+            <div className='mb-5 desc-input'>
               Esse é nome como você deverá ser mencionado nas compras ou registros.
             </div>
           </div>
-          <div className='form-group'>
-            <div className='form-row'>
-              <span className='span-input'>Email Público</span>
-              <div className='container-input'>
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder={`${email}@mail.com`}
-                />
-              </div>
+          <div className='w-full px-3 sm:w-1/2'>
+            <div className='mb-5'>
+              <label 
+                forhmtl='email'
+                className='mb-3 block text-base font-medium'>
+                  Email Público
+              </label>
+              <input
+                name='email'
+                className='form-control'
+                placeholder={`${email}@mail.com`}
+              />
             </div>
-            <div className='desc-input'>
+            <div className='mb-5 desc-input'>
               Você pode adicionar novos emails para facilitar a verificação.
             </div>
           </div>
-          <div className='form-group'>
-            <span className='span-input'>CPF</span>
+          <div className='w-full px-3 sm:w-1/2 mb-5'>
+            <label 
+              forhtml='cpf' 
+              className='mb-3 block text-base font-medium'>
+                CPF
+            </label>
             <div className='form-button'>
               <button 
                 type='submit'
-                className='btn-cad' 
+                name='cpf'
+                className='hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none' 
                 onClick={() => {}}>
                   Alterar o CPF
               </button>
             </div>
           </div>
-          <div className='form-group'>
-            <span className='span-input'>Senha</span>
+          <div className='w-full px-3 sm:w-1/2 mb-5'>
+            <label 
+              forhtml='password' 
+              className='mb-3 block text-base font-medium'>
+                Senha
+            </label>
             <div className='form-button'>
               <button
                 type='submit'
-                className='btn-cad' 
+                name='password'
+                className='hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none' 
                 onClick={() => {}}>
                   Reiniciar Senha
               </button>
             </div> 
           </div>
-          <div className='form-row'>
+          <div className='w-full px-3 mb-5'>
             <div className='form-button'>
               <button
                 type='submit'
-                className='btn-cad' 
+                className='hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 text-center text-base font-semibold text-white outline-none' 
                 onClick={() => {}}>
                   Torne-se um criador
               </button>
             </div>
-            <div className='form-button'>
+          </div>
+          <div className='w-full px-3 mb-5'>
+            <div className="form-button">
               <button
                 type='submit'
-                className='btn-cad' 
+                className='hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 text-center text-base font-semibold text-white outline-none' 
                 onClick={() => {}}>
                   Apagar sua conta
               </button>
-            </div>  
+            </div>
           </div>
         </div>
       </div>
