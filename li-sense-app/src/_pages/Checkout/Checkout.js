2c2cfc22b78610e5d542a824df0446638dbd4e03
Wanderson Bueno <wanolibueno@gmail.com>
2022-11-02 15:25:46 -0300

    criação de pagina de checkout

--- li-sense-app/src/_pages/Checkout/Checkout.js
+++ li-sense-app/src/_pages/Checkout/Checkout.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import Cart from '../../_components/Cart/Cart';
-import CheckoutComponent from '../../_components/Checkout/Checkout';
+import React from 'react'
+import Cart from '../../_components/Cart/Cart'
+import CheckoutComponent from '../../_components/Checkout/Checkout'
+import './checkout.css'
+
 export default function Checkout() {
   return (
     <>
-  <div className='flex items-center justify-around w-full'>
-  <div className='border '>
-  <Cart />
-    </div>
-    <div>
-      <CheckoutComponent />
-    </div>
-    </div>
+      <div className="page-checkout">
+        <div class="container-cart">
+          <h1>Carrinho de compras</h1>
+          <div class="container-detail">
+            <div class="list-titles">
+              <div>Produto</div>
+              <div>Preço</div>
+            </div>
+            <Cart />
+          </div>
+        </div>
+        <div class="container-check">
+          <CheckoutComponent />
+        </div>
+      </div>
     </>
   )
 }
