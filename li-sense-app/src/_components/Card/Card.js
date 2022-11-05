9f8462c2c842055c04a6e874b57f99309fa8be4a
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2022-11-05 03:20:10 -0300

    primeira versão estável do card

--- li-sense-app/src/_components/Card/Card.js
+++ li-sense-app/src/_components/Card/Card.js
@@ -4,6 +4,12 @@ import LisenseContext from "../../_context/LisenseContext";
 import "./Card.css";
 import Arrow from "./img/arrow.png";
 import { VscFile } from "react-icons/vsc";
+ import {
+   FaShoppingCart,
+   FaRegBookmark,
+   FaStar,
+   FaFireAlt,
+ } from "react-icons/fa";
 
 export default function Card(props) {
   const { data } = props;
@@ -15,7 +21,7 @@ export default function Card(props) {
   }
   return (
     <>
-      <div className="flex flex-col card overflow-hidden m-4" onClick={() => {setProduct(data)}}>
+      {/* <div className="flex flex-col productCard overflow-hidden m-4" onClick={() => {setProduct(data)}}>
         <div className="w-full flex justify-end items-center">
           <div className="vsicon flex items-center justify-center">
             <VscFile size={20} />
@@ -25,13 +31,41 @@ export default function Card(props) {
           <img src={data.url} className="img" alt="" />
           <div className="flex items-center">
             <div className="w-[80%] ml-4">
-              <p className="text-2xl text-white">{data.name}</p>
-              <p className="price">{data.price}</p>
-              <p className="desc">{data.description}</p>
+              <p className="productName">{data.name}</p>
+              <p className="productPrice">{data.price}</p>
+              <p className="productDesc">{data.description}</p>
             </div>
             <div className="arrows h-full w-[20%] -mt-8 -ml-8">
               <img src={Arrow} className="arrow"></img>
               <img src={Arrow} className="arrow"></img>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <div
+        className="productCard"
+        onClick={() => {
+          setProduct(data);
+        }}
+      >
+        <div className="productCard__mediaType">
+          <VscFile className={"productCard__Icon"} />
+        </div>
+        <FaShoppingCart className={"productCard__cart"} />
+        <FaRegBookmark className={"productCard__wishlist"} />
+        
+        <img src={data.url} className="productImage" alt="product-img" />
+
+        <div className="productList">
+          <div className="productCard__Content">
+            <h3 className="productName">{data.name}</h3>
+            <div className="displayStack__1">
+              <div className="productDec">
+                <p className="productDesc">{data.description}</p>
+              </div>
+              <div className="productPrice">
+                <p>{data.price}</p>
+              </div>
             </div>
           </div>
         </div>
@@ -46,9 +80,9 @@ export default function Card(props) {
       <div className="card" id="one">
         <div id='icon'>
           <div className='wrapper flex items-center mt-2'>
-          < VscFile size={30}/> 
+          < VscFile size={30}/>
           </div>
-        </div> 
+        </div>
         <img src={Image} className="view" alt="" />
 
         <p className="title">Nome</p>
@@ -57,7 +91,7 @@ export default function Card(props) {
           Lorem ipsum dolor sit aconsectetur adipisicing elit. Liobcaecati
           eligendi tempora nesciunt.
         </p>
-       
+
       </div>
     </div>
     </IconContext.Provider>
