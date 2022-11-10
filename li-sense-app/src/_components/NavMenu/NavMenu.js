0d4f1995f4b96949f9f5496ad5a20b4b7edafc70
||ȼhaiɳ <sfgfffs@gmail.com>
2022-11-10 12:47:37 -0300

    fix err in dev and set start s04

---
+++ li-sense-app/src/_components/NavMenu/NavMenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Filters from '../Filters/Filters'
+export default function NavMenu() {
+  return (
+    <>
+    <div className=''>
+      menu
+    
+        <Filters />
+  
+    </div>
+    </>
+  )
+}
