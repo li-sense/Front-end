ef523e8bf342c70f82081f029cb8d39d58ab8011
||ȼhaiɳ <sfgfffs@gmail.com>
2022-11-10 13:13:24 -0300

    add logic for edit product

--- li-sense-app/src/FakeData.js
+++ li-sense-app/src/FakeData.js
@@ -1,59 +1,69 @@
 export const data = [
   {
+    id:1,
     name: "TEST COM UMA GRANDE FRASE ENORME MEU DEUS QUE GRANDE",
     price: "R$ 99454.4",
     url: "https://i.pinimg.com/564x/f7/33/46/f73346e8aaf923c2fa915ad281ce9753.jpg",
     description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:2,
     name: "TO SEM IDEIA",
     price: "R$ 45",
     url: "https://i.pinimg.com/236x/5c/f5/f8/5cf5f8035fd93651f2f0c36de4b3aa9c.jpg",
     description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:3,
     name: "TA TARDE  JA",
     price: "R$ 454.5",
     url: "https://i.pinimg.com/236x/5b/28/48/5b284886db0baf8e689a55660d715a8f.jpg",
   description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:4,
     name: "OLHA QUE BACANA",
     price: "R$ 99.5",
     url: "https://i.pinimg.com/236x/40/df/d6/40dfd6e495bd8518a0e6c0f17f49ed57.jpg",
   description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:5,
     name: "BEM LEGAL ATE",
     price: "R$ 44,88",
     url: "https://i.pinimg.com/236x/4a/9d/4a/4a9d4a8e55fa6203fb0a6ed0b52f029c.jpg",
   description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:6,
     name: "VAI SABER NE",
     price: "R$ 22.4",
     url: "https://i.pinimg.com/564x/27/83/20/27832025615b7a6ba2c8c8bfefeff240.jpg",
   description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:7,
     name: "PAAII SANDUUU",
     price: "R$ 9999.9",
     url: "https://i.pinimg.com/564x/04/48/75/0448759aad8e2d47b90ba37f1e37712b.jpg",
   description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:8,
     name: "EU ODEIO O AKAINU",
     price: "R$ 9999.9",
     url: "https://i.pinimg.com/236x/de/47/aa/de47aa43c7027b70d9dfa8c024bfb4ea.jpg",
   description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:9,
     name: "EU ODEIO O AKAINU",
     price: "R$ 9999.9",
     url: "https://i.pinimg.com/236x/65/f7/76/65f776ea5db956ccd49de5242c81eb38.jpg",
   description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id:10,
     name: "EU ODEIO O AKAINU",
     price: "R$ 9999.9",
     url: "https://i.pinimg.com/564x/51/7e/59/517e5910e39c82cb4f5b93634bcfdb4d.jpg",
--- li-sense-app/src/_pages/ProductDetails/ProductDetails.js
+++ li-sense-app/src/_pages/ProductDetails/ProductDetails.js
@@ -1,12 +1,17 @@
 import React from "react";
 import LisenseContext from "../../_context/LisenseContext";
+import { useNavigate } from "react-router-dom";
 import './Product.css';
 export default function ProductDetails() {
   const { currentProduct } = React.useContext(LisenseContext);
   console.log(currentProduct);
+  const navigate = useNavigate()
   return (
     <>
     <div className="w-full h-full flex flex-col">
+      <button onClick={() => {navigate(`/product/${currentProduct.id}/edit`)}}>
+        Editar Produto
+      </button>
       <div className="flex items-center justify-around w-full">
       <div className="flex flex-col mr-4 items-center justify-center description">
         <h1>{currentProduct.name}</h1>
