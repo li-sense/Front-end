c5e3e0f5f8d33cdc5601857a541f927a704cebb5
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2022-11-19 19:34:18 -0300

    checkpoint - layout definido

--- li-sense-app/src/FakeData.js
+++ li-sense-app/src/FakeData.js
@@ -1,20 +1,27 @@
 export const data = [
   {
     id:1,
+    vendor: "Marcos Teixeira",
     name: "TEST COM UMA GRANDE FRASE ENORME MEU DEUS QUE GRANDE",
     price: "R$ 99454.4",
     url: "https://i.pinimg.com/564x/f7/33/46/f73346e8aaf923c2fa915ad281ce9753.jpg",
-    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    sale_type: "Compra vitalícia.",
+    type: "Projeto"
   },
   {
     id:2,
+    vendor: "Olivia Rodrigues",
     name: "TO SEM IDEIA",
     price: "R$ 45",
     url: "https://i.pinimg.com/236x/5c/f5/f8/5cf5f8035fd93651f2f0c36de4b3aa9c.jpg",
-    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    sale_type: "Compra vitalícia.",
+    type: "audio"
   },
   {
     id:3,
+    vendor: "Renata Dias",
     name: "TA TARDE  JA",
     price: "R$ 454.5",
     url: "https://i.pinimg.com/236x/5b/28/48/5b284886db0baf8e689a55660d715a8f.jpg",
@@ -22,6 +29,7 @@ export const data = [
   },
   {
     id:4,
+    vendor: "Tatiana Silva",
     name: "OLHA QUE BACANA",
     price: "R$ 99.5",
     url: "https://i.pinimg.com/236x/40/df/d6/40dfd6e495bd8518a0e6c0f17f49ed57.jpg",
@@ -29,6 +37,7 @@ export const data = [
   },
   {
     id:5,
+    vendor: "Barulho Talent",
     name: "BEM LEGAL ATE",
     price: "R$ 44,88",
     url: "https://i.pinimg.com/236x/4a/9d/4a/4a9d4a8e55fa6203fb0a6ed0b52f029c.jpg",
@@ -36,6 +45,7 @@ export const data = [
   },
   {
     id:6,
+    vendor: "Mais-e-mais",
     name: "VAI SABER NE",
     price: "R$ 22.4",
     url: "https://i.pinimg.com/564x/27/83/20/27832025615b7a6ba2c8c8bfefeff240.jpg",
@@ -43,6 +53,7 @@ export const data = [
   },
   {
     id:7,
+    vendor: "Futeboleiros",
     name: "PAAII SANDUUU",
     price: "R$ 9999.9",
     url: "https://i.pinimg.com/564x/04/48/75/0448759aad8e2d47b90ba37f1e37712b.jpg",
@@ -50,6 +61,7 @@ export const data = [
   },
   {
     id:8,
+    vendor: "Geek Culture",
     name: "EU ODEIO O AKAINU",
     price: "R$ 9999.9",
     url: "https://i.pinimg.com/236x/de/47/aa/de47aa43c7027b70d9dfa8c024bfb4ea.jpg",
@@ -57,6 +69,7 @@ export const data = [
   },
   {
     id:9,
+    vendor: "Anna Henrrietta",
     name: "EU ODEIO O AKAINU",
     price: "R$ 9999.9",
     url: "https://i.pinimg.com/236x/65/f7/76/65f776ea5db956ccd49de5242c81eb38.jpg",
@@ -64,6 +77,7 @@ export const data = [
   },
   {
     id:10,
+    vendor: "Geek Culture",
     name: "EU ODEIO O AKAINU",
     price: "R$ 9999.9",
     url: "https://i.pinimg.com/564x/51/7e/59/517e5910e39c82cb4f5b93634bcfdb4d.jpg",
--- li-sense-app/src/_pages/ProductDetails/ProductDetails.js
+++ li-sense-app/src/_pages/ProductDetails/ProductDetails.js
@@ -1,30 +1,89 @@
 import React from "react";
 import LisenseContext from "../../_context/LisenseContext";
-import { useNavigate } from "react-router-dom";
-import './Product.css';
+import "./Product.css";
+import { FaRegBookmark } from "react-icons/fa";
+
+
 export default function ProductDetails() {
   const { currentProduct } = React.useContext(LisenseContext);
   console.log(currentProduct);
-  const navigate = useNavigate()
   return (
     <>
-    <div className="w-full h-full flex flex-col">
-      <button onClick={() => {navigate(`/product/${currentProduct.id}/edit`)}}>
-        Editar Produto
-      </button>
-      <div className="flex items-center justify-around w-full">
-      <div className="flex flex-col mr-4 items-center justify-center description">
-        <h1>{currentProduct.name}</h1>
-        <p className="border p-4 w-48 cursor-pointer">
-          {currentProduct.price}
+      <div className="container-product">
+        {/* <div className="container-image-row">Cachorro</div> */}
+        <div className="container-image-product">
+          <div className="carrossel"></div>
+          <div className="container-image-prod">
+            <img className="bg" src={currentProduct.url} />
+          </div>
+        </div>
+        <div className="container-text-details">
+        <div className="product-details-wishlist">
+          <FaRegBookmark className={"productCard__wishlist"} />
+
+        </div>
+
+          <div className="h1-title-p">
+            <h1>{currentProduct.name}</h1>
+          </div>
+          <hr class="solid"></hr>
+            <div className="info">
+              <p class="field_desc">Id do produto: {currentProduct.id}</p>
+              <p class="field_desc">
+                Tipo de licença: {currentProduct.sale_type}
+              </p>
+              <p class="field_desc">Vendido por: {currentProduct.vendor}</p>
+            </div>
+          <div className="frame">
+            <p>Valor para compra em difinitivo:</p>
+            <p className="texto-prod">{currentProduct.price}</p>
+            <div>
+              
+            </div>
+            <button class="buy">Adquirir Licensa</button>
+          </div>
+        </div>
+      </div>
+      <div className="rest">
+        <hr class="solid"></hr>
+        <p className="details-prod">
+          <p className="desc-title">Descrição:</p>
+          {currentProduct.description}
         </p>
-        <p>{currentProduct.description}</p>
+
       </div>
-      <div className="image">
-      <img className="bg" src={currentProduct.url}/>
-      </div>
-      </div>
-    </div>
     </>
-  )
+  );
 }
+
+//PARTE QUE ESTAVA ANTES
+// import React from "react";
+// import LisenseContext from "../../_context/LisenseContext";
+// import { useNavigate } from "react-router-dom";
+// import './Product.css';
+// export default function ProductDetails() {
+//   const { currentProduct } = React.useContext(LisenseContext);
+//   console.log(currentProduct);
+//   const navigate = useNavigate()
+//   return (
+//     <>
+//     <div className="w-full h-full flex flex-col">
+//       <button onClick={() => {navigate(`/product/${currentProduct.id}/edit`)}}>
+//         Editar Produto
+//       </button>
+//       <div className="flex items-center justify-around w-full">
+//       <div className="flex flex-col mr-4 items-center justify-center description">
+//         <h1>{currentProduct.name}</h1>
+//         <p className="border p-4 w-48 cursor-pointer">
+//           {currentProduct.price}
+//         </p>
+//         <p>{currentProduct.description}</p>
+//       </div>
+//       <div className="image">
+//       <img className="bg" src={currentProduct.url}/>
+//       </div>
+//       </div>
+//     </div>
+//     </>
+//   )
+// }
