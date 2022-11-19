91e053e0cb4a0ef2985d3aa81c832160cf1cdb79
Wanderson Bueno <wanolibueno@gmail.com>
2022-11-19 02:11:03 -0300

    page search product color

--- li-sense-app/src/_components/Footer/footer.js
+++ li-sense-app/src/_components/Footer/footer.js
@@ -1,40 +1,43 @@
-import React from "react";
-import "./footer.css";
-import Logo from "../../_assets/img/logo.png";
+import React from 'react'
+import './footer.css'
+import Logo from '../../_assets/img/logo.png'
 import {
   FaFacebookSquare,
   FaInstagram,
   FaLinkedin,
-  FaGithubSquare,
-} from "react-icons/fa";
+  FaGithubSquare
+} from 'react-icons/fa'
 
 const footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-logo">
-        <a>
-          <img src={Logo} alt="" />
-        </a>
+    <>
+      <div className="divider-1"></div>
+      <div className="footer">
+        <div className="footer-logo">
+          <a>
+            <img src={Logo} alt="" />
+          </a>
+        </div>
+        <div className="footer-central">
+          <p>© 2022 Todos os direitos reservados</p>
+        </div>
+        <div className="footer-social">
+          <a>
+            <FaLinkedin />
+          </a>
+          <a>
+            <FaGithubSquare />
+          </a>
+          <a>
+            <FaFacebookSquare />
+          </a>
+          <a>
+            <FaInstagram />
+          </a>
+        </div>
       </div>
-      <div className="footer-central">
-        <p>© 2022 Todos os direitos reservados</p>
-      </div>
-      <div className="footer-social">
-        <a>
-          <FaLinkedin />
-        </a>
-        <a>
-          <FaGithubSquare />
-        </a>
-        <a>
-          <FaFacebookSquare />
-        </a>
-        <a>
-          <FaInstagram />
-        </a>
-      </div>
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default footer;
+export default footer
