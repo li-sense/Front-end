c77a9faa0335fd6d6465857c1d8ddadf157f9578
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2022-11-21 21:18:48 -0300

    tela de detalhes do produto
    formatada para diferentes tamanhos de tela

--- li-sense-app/src/_pages/ProductDetails/ProductDetails.js
+++ li-sense-app/src/_pages/ProductDetails/ProductDetails.js
@@ -11,9 +11,7 @@ export default function ProductDetails() {
     <>
       <div className="container-product">
         <div className="container-image-product">
-          {/* <div className="container-image-prod"> */}
           <img className="img-details" src={currentProduct.url} />
-          {/* </div> */}
         </div>
 
         <div className="container-product-details">
@@ -44,46 +42,12 @@ export default function ProductDetails() {
       </div>
 
 
-      <div className="rest">
-        <hr class="solid"></hr>
-        <p className="details-product">
-          <p className="title-desc">Descrição:</p>
-          {currentProduct.description}
-        </p>
-      </div>
+      <hr class="solid"></hr>
+      <p className="details-product">
+        <p className="title-desc">Descrição:</p>
+        {currentProduct.description}
+      </p>
       <hr class="solid"></hr>
     </>
   );
 }
-
-//PARTE QUE ESTAVA ANTES
-// import React from "react";
-// import LisenseContext from "../../_context/LisenseContext";
-// import { useNavigate } from "react-router-dom";
-// import './Product.css';
-// export default function ProductDetails() {
-//   const { currentProduct } = React.useContext(LisenseContext);
-//   console.log(currentProduct);
-//   const navigate = useNavigate()
-//   return (
-//     <>
-//     <div className="w-full h-full flex flex-col">
-//       <button onClick={() => {navigate(`/product/${currentProduct.id}/edit`)}}>
-//         Editar Produto
-//       </button>
-//       <div className="flex items-center justify-around w-full">
-//       <div className="flex flex-col mr-4 items-center justify-center description">
-//         <h1>{currentProduct.name}</h1>
-//         <p className="border p-4 w-48 cursor-pointer">
-//           {currentProduct.price}
-//         </p>
-//         <p>{currentProduct.description}</p>
-//       </div>
-//       <div className="image">
-//       <img className="bg" src={currentProduct.url}/>
-//       </div>
-//       </div>
-//     </div>
-//     </>
-//   )
-// }
